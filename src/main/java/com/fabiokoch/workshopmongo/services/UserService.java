00c381eeddf298f705cb941bac4dbf6101526f99
fabiokoch <fabiokochdaluz@hotmail.com>
2019-06-26 08:59:01 -0300

    Deletando usuário com DELETE

--- src/main/java/com/fabiokoch/workshopmongo/services/UserService.java
+++ src/main/java/com/fabiokoch/workshopmongo/services/UserService.java
@@ -26,8 +26,13 @@ public class UserService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 	
-	public User inset(User obj) {
+	public User insert(User obj) {
 		return repo.insert(obj);
+	}
+	
+	public void delete(String id) {
+		findById(id);
+		repo.deleteById(id);
 	}
 	
 	public User fromDTO(UserDTO objDto) {
